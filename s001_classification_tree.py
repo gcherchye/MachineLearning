fba30d7dad06b98a096433ca66a3408d47c6d13d
Goodyear <g.cherchye@gmail.com>
2021-03-08 00:58:49 +0100

    feat(tree): finalize base tree model

--- s001_classification_tree.py
+++ s001_classification_tree.py
@@ -1,6 +1,10 @@
 """Decision tree classifier"""
 from __future__ import absolute_import
 
+import matplotlib.pyplot as plt
+from scikitplot.metrics import plot_confusion_matrix
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
 from s000_import_data import get_data, data_prep
 
 
@@ -10,3 +14,18 @@ data = get_data()
 x_train, x_test, y_train, y_test = data_prep(data)
 
 # Model definition
+tree = DecisionTreeClassifier()
+
+# Fit model
+tree.fit(x_train, y_train)
+
+# Test model
+y_pred = tree.predict(x_test)
+
+# Confusion matrix
+plot_confusion_matrix(y_test, y_pred)
+plt.show()
+
+# Show tree
+plot_tree(tree)
+plt.show()
