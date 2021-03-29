9b93922d8c8d2385717fbdcd6a9410efb2e93a57
Goodyear <g.cherchye@gmail.com>
2021-03-29 21:03:10 +0200

    refactor(overall): clean tree

--- s001_classification_tree.py
+++ s001_classification_tree.py
@@ -2,10 +2,9 @@
 from __future__ import absolute_import
 
 import matplotlib.pyplot as plt
-from scikitplot.metrics import plot_confusion_matrix
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 
-from s000_import_data import get_data, data_prep
+from tools.import_data import get_data, data_prep
 
 
 # Import data
@@ -14,7 +13,9 @@ data = get_data()
 x_train, x_test, y_train, y_test = data_prep(data)
 
 # Model definition
-tree = DecisionTreeClassifier()
+tree = DecisionTreeClassifier(
+    max_depth=3
+)
 
 # Fit model
 tree.fit(x_train, y_train)
@@ -22,9 +23,6 @@ tree.fit(x_train, y_train)
 # Test model
 y_pred = tree.predict(x_test)
 
-# Confusion matrix
-plot_confusion_matrix(y_test, y_pred)
-plt.show()
 
 # Show tree
 plot_tree(tree)
